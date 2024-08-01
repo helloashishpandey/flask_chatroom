c558d28d1def9b5091355351b2b652bc3f1f0079
Ashish Pandey <63122223+helloashishpandey@users.noreply.github.com>
2024-08-02 03:11:05 +0530

    Update postmain.py

--- postmain.py
+++ postmain.py
@@ -157,4 +157,6 @@ def handle_message(data):
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    socketio.run(app, debug=True, allow_unsafe_werkzeug=True)
+    port = int(os.environ.get('PORT', 5000))
+    socketio.run(app, host='0.0.0.0', port=port, allow_unsafe_werkzeug=True)
+
